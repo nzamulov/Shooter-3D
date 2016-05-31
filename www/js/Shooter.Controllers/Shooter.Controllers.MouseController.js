a5cb8ebf1589735b43d1e4ace604d24f68bbc7d4
Nikolay Zamulov <kolia8@mail.ru>
2016-05-31 18:07:36 +0300

    Refactoring #4.

--- www/js/Shooter.Controllers/Shooter.Controllers.MouseController.js
+++ www/js/Shooter.Controllers/Shooter.Controllers.MouseController.js
@@ -2,9 +2,12 @@
 
 Shooter.namespace("Shooter.Controllers");
 
-Shooter.Controllers.MouseController = class {
+import AbstractController from './Shooter.Controllers.AbstractController.js';
+
+Shooter.Controllers.MouseController = class extends AbstractController {
 
 	constructor(player) {
+		super();
 
 		this.player = player;
 
@@ -17,37 +20,39 @@ Shooter.Controllers.MouseController = class {
 		this.yawObject.add(this.pitchObject);
 
 		this.PI_2 = Math.PI / 2;
+	}
+
+	attachEvents() {
+
+		this.onMouseMove = (event) => {
+
+			let movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+			let movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
+			this.yawObject.rotation.y -= movementX * 0.002;
+			this.pitchObject.rotation.x -= movementY * 0.002;
+
+			this.pitchObject.rotation.x = Math.max( -this.PI_2, Math.min( this.PI_2, this.pitchObject.rotation.x ) );
+
+			let direction = new THREE.Vector3(0, 0, -1);
+			let rotation = new THREE.Euler(0, 0, 0, "YXZ");
+			let lookAt = new THREE.Vector3();
+
+			rotation.set(this.pitchObject.rotation.x, this.yawObject.rotation.y, 0);
+
+			lookAt.copy(direction).applyEuler(rotation);
+
+			lookAt.x += this.player.camera.position.x;
+			lookAt.y += this.player.camera.position.y;
+			lookAt.z += this.player.camera.position.z;
+
+			this.player.camera.lookAt(lookAt);
+
+		}
 
 		let self = this;
 
-		document.addEventListener('mousemove', function(event) { self.onMouseMove(event); }, false);
-
-	}
-
-	onMouseMove(event) {
-
-		let movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-		let movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-
-		this.yawObject.rotation.y -= movementX * 0.002;
-		this.pitchObject.rotation.x -= movementY * 0.002;
-
-		this.pitchObject.rotation.x = Math.max( -this.PI_2, Math.min( this.PI_2, this.pitchObject.rotation.x ) );
-
-		let direction = new THREE.Vector3(0, 0, -1);
-		let rotation = new THREE.Euler(0, 0, 0, "YXZ");
-		let lookAt = new THREE.Vector3();
-
-		rotation.set(this.pitchObject.rotation.x, this.yawObject.rotation.y, 0);
-
-		lookAt.copy(direction).applyEuler(rotation);
-
-		lookAt.x += this.player.camera.position.x;
-		lookAt.y += this.player.camera.position.y;
-		lookAt.z += this.player.camera.position.z;
-
-		this.player.camera.lookAt(lookAt);
-
+		document.addEventListener('mousemove', (event) => { self.onMouseMove(event); }, false);
 	}
 
 	getObject() {
