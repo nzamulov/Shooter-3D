2f9968c9388a82b09d11ab2b9a9be107e5d085e8
Nikolay Zamulov <kolia8@mail.ru>
2016-06-01 22:18:51 +0300

    Extend the requestAnimationFrame file.

--- www/js/Shooter.Utils/Shooter.Utils.requestAnimationFrame.js
+++ www/js/Shooter.Utils/Shooter.Utils.requestAnimationFrame.js
@@ -6,7 +6,9 @@ Shooter.Utils.requestAnimationFrame = (() => {
 	return  window.requestAnimationFrame       ||
 			window.webkitRequestAnimationFrame ||
 			window.mozRequestAnimationFrame    ||
-			function( callback ){
+			window.oRequestAnimationFrame 	   ||
+        	window.msRequestAnimationFrame 	   ||
+			function(callback) {
 				window.setTimeout(callback, 1000 / 60);
 			};
 })();
