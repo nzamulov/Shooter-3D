febb4679de4e38d76f2451e34a7f7c7f5cfed11b
Nikolay Zamulov <kolia8@mail.ru>
2016-06-12 01:03:09 +0300

    Refactoring.

--- www/js/Shooter.Entities/Shooter.Entities.Builders/Shooter.Entities.Builders.AbstractBuilder/Shooter.Entities.Builders.AbstractBuilder.js
+++ www/js/Shooter.Entities/Shooter.Entities.Builders/Shooter.Entities.Builders.AbstractBuilder/Shooter.Entities.Builders.AbstractBuilder.js
@@ -39,6 +39,45 @@ Shooter.Entities.Builders.AbstractBuilder = class {
 	setPosition(position) { }
 	setRotation(rotation) { }
 
+
+	/* TEXTURE NORMALIZATION */
+	assignUVs(geometry) {
+
+	    geometry.computeBoundingBox();
+
+	    let max = geometry.boundingBox.max;
+	    let min = geometry.boundingBox.min;
+
+	    let offset  = new THREE.Vector3(0 - min.x, 0 - min.y, 0 - min.z);
+	    let range   = new THREE.Vector3(max.x - min.x, max.y - min.y, max.z - min.z);
+
+	    geometry.faceVertexUvs[0] = [];
+
+	    let faces = geometry.faces;
+
+	    for (let i = 0; i < geometry.faces.length ; i++) {
+
+	      let v1 = geometry.vertices[faces[i].a];
+	      let v2 = geometry.vertices[faces[i].b];
+	      let v3 = geometry.vertices[faces[i].c];
+
+	      if(v1.x === v2.x && v2.x === v3.x) {
+		      geometry.faceVertexUvs[0].push([
+		        new THREE.Vector2( ( v1.z + offset.z ) / range.z , ( v1.y + offset.y ) / range.y ),
+		        new THREE.Vector2( ( v2.z + offset.z ) / range.z , ( v2.y + offset.y ) / range.y ),
+		        new THREE.Vector2( ( v3.z + offset.z ) / range.z , ( v3.y + offset.y ) / range.y )
+		      ]);
+	      } else {
+		      geometry.faceVertexUvs[0].push([
+		        new THREE.Vector2( ( v1.x + offset.x ) / range.x , ( v1.y + offset.y ) / range.y ),
+		        new THREE.Vector2( ( v2.x + offset.x ) / range.x , ( v2.y + offset.y ) / range.y ),
+		        new THREE.Vector2( ( v3.x + offset.x ) / range.x , ( v3.y + offset.y ) / range.y )
+		      ]);
+	      }
+	    }
+
+	    geometry.uvsNeedUpdate = true;
+	}
 };
 
 export default Shooter.Entities.Builders.AbstractBuilder;
